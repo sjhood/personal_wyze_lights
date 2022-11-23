75a23caf382958529254da801c770a7c25bfb734
sjhood <sadie.j.hood@gmail.com>
2022-11-22 22:05:03 -0800

    adding dockerfile information to containerize this process

---
+++ wyze/entrypoint_script.sh
@@ -0,0 +1,3 @@
+echo ${WYZE_EMAIL}
+echo ${WYZE_PASSWORD}
+python3.10 scripts/main.py
--- wyze/scripts/main.py
+++ wyze/scripts/main.py
@@ -6,11 +6,16 @@ from modify_lighting import *
 from helpers import *
 
 def main():
-    client = Client(email=os.environ['WYZE_EMAIL'], password=os.environ['WYZE_PASSWORD'])
-    light_bulbs = get_bulbs(client)
+    if os.environ.get('WYZE_EMAIL') and os.environ.get('WYZE_PASSWORD'):
+        client = Client(email=os.environ['WYZE_EMAIL'], password=os.environ['WYZE_PASSWORD'])
+        light_bulbs = get_bulbs(client)
 
-    # response = change_lighting_pattern(client, light_bulbs, get_lighting("bi-color-1")["colors"], get_lighting("rainbow")["brightness"])
-    change_lighting_environment(client, gaming_setting())
-    # turn_off_all_lights(client, light_bulbs)
+        # response = change_lighting_pattern(client, light_bulbs, get_lighting("bi-color-1")["colors"], get_lighting("rainbow")["brightness"])
+        change_lighting_environment(client, gaming_setting())
+        # turn_off_all_lights(client, light_bulbs)
+    else:
+        print("Need to give email and password to authenticate")
+        print("export WYZE_EMAIL=<email>")
+        print("export WYZE_PASSWORD=<password>")
 
 main()
